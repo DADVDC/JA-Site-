d4dfe6895d39d82af15c0ea45f46742b2910ca30
littleblack111 <littleblack11111@gmail.com>
2025-02-17 17:30:46 +0800

    inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 
 import Nav from "@/components/nav";
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-r from-[#58C3E0] to-[#FB7EC4]">
+      <body className={`bg-gradient-to-r from-[#58C3E0] to-[#FB7EC4] ${inter.className}`}>
         <Nav />
         {children}
       </body>
