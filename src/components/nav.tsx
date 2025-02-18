3a5b0996df99533c9217e33bc6a729bf8d800dc3
littleblack111 <littleblack11111@gmail.com>
2025-02-18 14:21:05 +0800

    fix(nav): font size too big

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -10,7 +10,7 @@ import {
 
 export default function Nav() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 mt-10 min-h-16 inset-shadow-2xs px-4 md:px-[50px] bg-transparent drop-shadow-lg flex flex-1 items-center justify-between 2xl:[&_*]:text-6xl md:text-3xl backdrop-blur-xl">
+    <div className="fixed top-0 left-0 right-0 z-50 mt-10 min-h-16 inset-shadow-2xs px-4 md:px-[50px] bg-transparent drop-shadow-lg flex flex-1 items-center justify-between 2xl:[&_*]:text-5xl md:text-3xl backdrop-blur-xl">
       <Link href="/about">About us</Link>
       <div className="hidden md:flex space-x-10 flex-1 ml-10">
         <Link href="/game">GAME PLAY</Link>
