0a66c800eeb2fa276a98e1ebea2cc7eaa6044370
littleblack111 <littleblack11111@gmail.com>
2025-02-18 20:07:32 +0800

    responsive design & cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
         dot2dot
       </h1>
 
-      <div className="absolute left-10 top-21 md:left-10 md:top-20">
+      <div className="absolute left-10 top-[10rem]">
         <Image src="/home3.png" alt="Card 3" width={0} height={0} sizes="250vw" className="w-full h-auto" />
       </div>
       <div className="absolute right-10 top-[10rem]">
         <Image src="/home2.png" alt="Card 2" width={0} height={0} sizes="250vw" className="w-full h-auto" />
       </div>
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-1">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-1 max-md:bottom-52">
         <Image src="/home1.png" alt="Card 1" width={0} height={0} sizes="250vw" className="w-full h-auto" />
       </div>
     </section>
