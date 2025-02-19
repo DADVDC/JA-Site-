8cf913d1b0f6b8f0282508cd2c6ab596ca24ce0b
littleblack111 <littleblack11111@gmail.com>
2025-02-19 17:56:52 +0800

    fixup! z-index use proper tailwind

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -4,10 +4,10 @@ export default function Section({ children, ...props }: HTMLAttributes<HTMLEleme
   return (
     <section className="relative min-h-screen flex flex-col items-center justify-center w-full h-full" {...props}>
       {children}
-      <img src="/lvup4.png" alt="Card 4" className="absolute left-10 bottom-1 max-md:h-40 max-w-1/2 z-[-10]" />
-      <img src="/lvup3.png" alt="Card 3" className="absolute h-auto max-md:h-40 right-0 top-[10rem] z-[-10]" />
-      <img src="/lvup2.png" alt="Card 2" className="absolute max-md:h-40 max-w-1/2 h-auto right-0 bottom-1 z-[-10]" />
-      <img src="/lvup1.png" alt="Card 1" className="absolute left-10 top-20 max-md:h-40 max-w-1/2 z-[-10]" />
+      <img src="/lvup4.png" alt="Card 4" className="absolute left-10 bottom-1 max-md:h-40 max-w-1/2 -z-10" />
+      <img src="/lvup3.png" alt="Card 3" className="absolute h-auto max-md:h-40 right-0 top-[10rem] -z-10" />
+      <img src="/lvup2.png" alt="Card 2" className="absolute max-md:h-40 max-w-1/2 h-auto right-0 bottom-1 -z-10" />
+      <img src="/lvup1.png" alt="Card 1" className="absolute left-10 top-20 max-md:h-40 max-w-1/2 -z-10" />
     </section>
   )
 
